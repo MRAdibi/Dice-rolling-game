2044275873f56eed81667d80cc636e850f87ba6f
AlirezaRezaie <rezaiealireza884@gmail.com>
2022-10-21 00:00:37 +0330

    some code refactor and increased code readability

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,8 @@
 // main variables {
 
 // player instances
-let player_1 = new Player(document.getElementById("player1"));
-let player_2 = new Player(document.getElementById("player2"));
+let player_1 = new Player(document.getElementById("player1"), "player1");
+let player_2 = new Player(document.getElementById("player2"), "player2");
 
 // buttons
 const nameGameBtn = document.querySelector(".btn--new");
@@ -15,62 +15,79 @@ const img = document.querySelector(".dice");
 // active player init
 let activePlayer = player_1;
 
+let clientPlayer = undefined;
+
+//game type
+let online = true;
+
 // } end of main variables
 
 // connecting client socket to the websocket server
-let socket = new WebSocket("ws://localhost:8001");
+let socket = io("http://localhost:8000", {
+  transports: ["websocket", "polling", "flashsocket"],
+});
 
-// on connection opened event
-socket.onopen = (e) => {
-  console.log("[open] Connection established");
-  // creating user unique id
-
-  // sending a unique id to
-  socket.send(
-    JSON.stringify({
-      type: "get_token",
-    })
-  );
-};
-
-socket.onmessage = (e) => {
-  const jsonData = JSON.parse(e.data);
-  switch (jsonData.type) {
-    case "token":
-      console.log("token is " + jsonData.data);
-
-    default:
-      console.log(jsonData);
+socket.on("deter player", (msg) => {
+  if (msg == "player1") {
+    clientPlayer = player_1;
+  } else {
+    clientPlayer = player_2;
   }
-};
+});
+
+socket.on("update score", (msg) => {
+  activePlayer.score = msg.score;
+  activePlayer.scoreElement.textContent = msg.score;
+});
+
+socket.on("update current score", (msg) => {
+  console.log("f");
+  activePlayer.currentScore = msg.score;
+  activePlayer.currentScoreElement.textContent = msg.score;
+});
+
+socket.on("roll dice", (value) => {
+  setImg(value);
+});
+
+socket.on("switch player", (msg) => {
+  console.log(msg);
+  if (msg == "player1") {
+    activePlayer = player_1;
+  } else {
+    activePlayer = player_2;
+  }
+  player_1.root_element.classList.toggle("player--active");
+  player_2.root_element.classList.toggle("player--active");
+});
 
 // image setter function based on dice number
 const setImg = (number) => {
   img.src = `Img/dice-${number}.png`;
 };
 
-// calculate the current currentScore
-const addDice = (number) => {
-  activePlayer.setCurrentScore(activePlayer.currentScore + number);
-};
-
 // change style whene a player loss his acticePlayer
 const switchPlayer = () => {
   player_1.root_element.classList.toggle("player--active");
   player_2.root_element.classList.toggle("player--active");
-  if (activePlayer == player_1) {
-    activePlayer = player_2;
-  } else if (activePlayer == player_2) {
-    activePlayer = player_1;
-  }
+
+  activePlayer == player_1
+    ? (activePlayer = player_2)
+    : (activePlayer = player_1);
+
+  socket.emit("switch player", activePlayer.player_name);
 };
 
 // check the winner
 const checkWinner = () => {
   if (player_1.score >= 100) {
     player_1.root_element.classList.add("player--winner");
+    holdBtn.disabled = true;
+    rollDiceBtn.disabled = true;
   } else if (player_2.score >= 100) {
     player_2.root_element.classList.add("player--winner");
+    holdBtn.disabled = true;
+    rollDiceBtn.disabled = true;
   }
 };
 
@@ -78,43 +95,23 @@ const checkWinner = () => {
 
 // roll dice event (main game system)
 rollDiceBtn.addEventListener("click", () => {
-  if (player_1.score <= 100 && player_2.score <= 100) {
-    let generateNumber = Math.trunc(Math.random() * 6) + 1;
-    if (generateNumber == 1) {
-      let number = 1;
-      setImg(number);
-      if (activePlayer == player_1) {
-        activePlayer.setCurrentScore(0);
-
-        switchPlayer();
-      } else if (activePlayer == player_2) {
-        activePlayer.setCurrentScore(0);
-        switchPlayer();
-      }
-      checkWinner();
-    } else {
-      setImg(generateNumber);
-      addDice(generateNumber);
-      checkWinner();
-    }
+  let generateNumber = Math.trunc(Math.random() * 6) + 1;
+  setImg(generateNumber);
+  socket.emit("roll dice", generateNumber);
+  // switch player if roll dice is 1
+  if (generateNumber == 1) {
+    activePlayer.setCurrentScore(0);
+    switchPlayer();
+  } else {
+    activePlayer.setCurrentScore(activePlayer.currentScore + generateNumber);
   }
 });
 
 // holding the score event
 holdBtn.addEventListener("click", () => {
-  if (player_1.score <= 100 && player_2.score <= 100) {
-    if (activePlayer == player_1) {
-      player_1.setScore(player_1.currentScore + player_1.score);
-      activePlayer.setCurrentScore(0);
-      switchPlayer();
-      checkWinner();
-    } else if (activePlayer == player_2) {
-      player_2.setScore(player_2.currentScore + player_2.score);
-      activePlayer.setCurrentScore(0);
-      switchPlayer();
-      checkWinner();
-    }
-  }
+  activePlayer.setScore(activePlayer.currentScore + activePlayer.score);
+  activePlayer.setCurrentScore(0);
+  switchPlayer();
 });
 
 // Restarting the game event
@@ -126,15 +123,16 @@ nameGameBtn.addEventListener("click", () => {
   player_2.setCurrentScore(0);
 
   img.src = "";
+
+  holdBtn.disabled = false;
+  rollDiceBtn.disabled = false;
+
   activePlayer = player_1;
   player1.classList.add("player--active");
   player2.classList.remove("player--active");
 
-  if (player1.classList.contains("player--winner")) {
-    player1.classList.remove("player--winner");
-  } else if (player2.classList.contains("player--winner")) {
-    player2.classList.remove("player--winner");
-  }
+  player1.classList.remove("player--winner");
+  player2.classList.remove("player--winner");
 });
 
 // } end of the game events
