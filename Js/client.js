42077e56366cb281ce423d34702582168335265e
AlirezaRezaie <rezaiealireza884@gmail.com>
2022-10-22 23:17:11 +0330

    init state on new player enters

--- Js/client.js
+++ Js/client.js
@@ -3,12 +3,30 @@ let socket = io("http://localhost:8000", {
   transports: ["websocket", "polling", "flashsocket"],
 });
 
-socket.on("deter player", (msg) => {
-  if (msg == "player1") {
-    clientPlayer = player_1;
-  } else {
-    clientPlayer = player_2;
+const getPlayer = (name) => {
+  if (name == "player1" || name == "player_1") return player_1;
+  else if (name == "player2" || name == "player_2") return player_2;
+};
+
+socket.on("init game state", (msg, active) => {
+  console.log(msg);
+
+  player_1.setScore(msg.player_1.score, false);
+  player_1.setCurrentScore(msg.player_1.currentScore, false);
+
+  player_2.setScore(msg.player_2.score, false);
+  player_2.setCurrentScore(msg.player_2.currentScore, false);
+  console.log(active.name);
+  if (!active.name == "player_1") {
+    player_1.root_element.classList.toggle("player--active");
+    player_2.root_element.classList.toggle("player--active");
   }
+
+  activePlayer = getPlayer(active.name);
+});
+
+socket.on("deter player", (msg) => {
+  activePlayer = getPlayer(msg);
 });
 
 socket.on("update score", (msg) => {
@@ -28,11 +46,7 @@ socket.on("roll dice", (value) => {
 
 socket.on("switch player", (msg) => {
   console.log(msg);
-  if (msg == "player1") {
-    activePlayer = player_1;
-  } else {
-    activePlayer = player_2;
-  }
+  activePlayer = getPlayer(msg);
   player_1.root_element.classList.toggle("player--active");
   player_2.root_element.classList.toggle("player--active");
 });
